7061e6c3285684b4bd878cb6b5fbda611610e0d2
Logan Snopek <LSnopek@lcs.on.ca>
2022-11-18 14:17:46 -0500

    Initial Commit

---
+++ ShrinkingSurecles/ShrinkingSureclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShrinkingSureclesApp.swift
+//  ShrinkingSurecles
+//
+//  Created by Logan Snopek on 2022-11-18.
+//
+
+import SwiftUI
+
+@main
+struct ShrinkingSureclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
